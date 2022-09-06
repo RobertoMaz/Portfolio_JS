a013d496878dfda3ec3f5d7c3e8d8b4c67d01b4f
Roberto Mazzarello <rjmazzarello@gmail.com>
2022-09-06 17:14:04 -0300

    version 06-09 17.14

--- Portfolio_JS/js/script.js
+++ Portfolio_JS/js/script.js
@@ -21,3 +21,38 @@
     });
 })(document);
 
+
+/**** ConctactForm ****/
+((d)=>{
+    const $form = d.querySelector(".contact-form"),
+        $loader = d.querySelector(".contact-form-loader"),
+        $response = d.querySelector(".contact-form-response");
+
+    $form.addEventListener("submit", (e) => {
+
+        e.preventDefault();
+
+        $loader.classList.remove("none");
+
+        fetch("https://formsubmit.co/ajax/rjmazzarello@gmail.com", {
+            method: "POST",
+            body: new FormData(e.target)
+        })
+        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+        .then(json => {
+            location.hash = "#gracias";
+            $form.reset();
+        })
+        .catch((err) => {
+            console.log(err);
+            let message = err.statusText || "Ocurrio un error al enviar, intenta nuevamente."
+            $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+        }).finally(() => {
+            $loader.classList.add("none");
+            setTimeout(() => {
+                location.hash = "#cerrar";
+            }, 3000)
+        });
+    });
+})(document);
+
